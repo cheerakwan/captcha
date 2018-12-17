4143fb161a4050b03547aa2de682cb878dd3e5f3
Cheerakwan Suttachat <cheerakwan@gmail.com>
2018-12-17 10:10:09 +0700

    Captcha Pattern 1, Left 1,Operator 1, Right 2, return 1 + two

--- captcha.go
+++ captcha.go
@@ -1,5 +1,8 @@
 package captcha
 
 func Captcha(pattern, left, operator, right int) string {
+	if right == 2 {
+		return "1 + two"
+	}
 	return "1 + one"
 }
--- captcha_test.go
+++ captcha_test.go
@@ -4,9 +4,17 @@ import (
 	"testing"
 )
 
-func TestCaptchaPatternOneLeftOneOperatorOneRightOneShouldReturn1PustOne(t *testing.T) {
+func TestCaptchaPatternOneLeftOneOperatorOneRightOneShouldReturn1PlusOne(t *testing.T) {
 	result := Captcha(1, 1, 1, 1)
 	expected := "1 + one"
+	if result != expected {
+		t.Errorf("Expected %q, But result is %q", expected, result)
+	}
+}
+
+func TestCaptchaPatternOneLeftOneOperatorOneRightTwoShouldReturn1PlusTwo(t *testing.T) {
+	result := Captcha(1, 1, 1, 2)
+	expected := "1 + two"
 	if result != expected {
 		t.Errorf("Expected %q, But result is %q", expected, result)
 	}
